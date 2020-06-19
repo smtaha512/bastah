6b1ec17c1e2695a9c96d146fa5261930afac7397
smtaha512 <s.m.taha10@gmail.com>
2020-06-19 10:33:02 +0500

    feat: close menu on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 import { Observable, of } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { filter, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +13,30 @@ import { filter, map } from 'rxjs/operators';
 export class AppComponent implements OnInit {
   shouldDisableMenu$: Observable<boolean> = of(false);
 
-  constructor(private readonly router: Router) {}
+  constructor(
+    private readonly router: Router,
+    private readonly menuController: MenuController
+  ) {}
 
   ngOnInit(): void {
+    this.toggleSideMenuStateOnRouteChange();
+    this.closeMenuOnRouteChange();
+  }
+
+  private toggleSideMenuStateOnRouteChange() {
     this.shouldDisableMenu$ = this.router.events.pipe(
       filter((event) => event instanceof NavigationEnd),
       map((event: NavigationEnd) => event.urlAfterRedirects),
       map((url) => url === '/home')
     );
+  }
+
+  private closeMenuOnRouteChange() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationStart),
+        tap(() => this.menuController.close())
+      )
+      .subscribe();
   }
 }
