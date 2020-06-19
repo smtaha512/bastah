75fdc9e94a2eb0abe55ccd6ae9c300d47366c964
smtaha512 <s.m.taha10@gmail.com>
2020-06-19 13:35:47 +0500

    build: add basic service worker configurations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuModule } from './components/menu/menu.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +18,7 @@ import { MenuModule } from './components/menu/menu.module';
     IonicModule.forRoot(),
     AppRoutingModule,
     MenuModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
