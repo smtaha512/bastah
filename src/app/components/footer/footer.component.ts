123412964675ff5eec240c34c04fde01843cb4a9
smtaha512 <s.m.taha10@gmail.com>
2020-07-02 18:02:42 +0500

    feat: add zoom in and zoom functionality

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  Component,
+  Inject,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { filter, first, scan, shareReplay, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent implements OnInit {
-  constructor() {}
+export class FooterComponent implements OnInit, OnDestroy {
+  // timesZoom define how much we have zoomed. Example `1` times, `2` times etc
+  private readonly timesZoom$$ = new BehaviorSubject(1);
+  readonly timesZoom$ = this.timesZoom$$
+    .asObservable()
+    .pipe(shareReplay({ refCount: true, bufferSize: 1 }));
+
+  private readonly subscription = new Subscription();
+  constructor(
+    private readonly renderer: Renderer2,
+    @Inject(DOCUMENT) private readonly document: Document
+  ) {}
 
   ngOnInit() {}
+
+  zoom(inOut: number) {
+    this.subscription.unsubscribe();
+    this.subscription.add(
+      this.timesZoom$
+        .pipe(
+          first(),
+          filter((timesZoom) => this.canZoom(timesZoom, inOut)),
+          scan((acc, value) => acc + value, inOut),
+          tap((timesZoom) => this.timesZoom$$.next(timesZoom)),
+          tap((timesZoom) => this.updateFontSize(16 + timesZoom * 2 - 2))
+        )
+        .subscribe()
+    );
+  }
+
+  private canZoom(timesZoom: number, inOutFactor: number) {
+    return (
+      // can zoom in
+      (timesZoom < 5 && inOutFactor === 1) ||
+      // can zoom out
+      (timesZoom > 1 && inOutFactor === -1)
+    );
+  }
+
+  private updateFontSize(fontSize: number) {
+    this.renderer.setAttribute(
+      this.document.body.parentElement,
+      'style',
+      `--root-font-size: ${fontSize}px`
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
