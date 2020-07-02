69a15605d283548992979301e1ec4e9b44d6a128
smtaha512 <s.m.taha10@gmail.com>
2020-07-02 14:43:33 +0500

    fix: move urdu after arabic to correct place

--- src/app/pipes/arabic/arabic.pipe.ts
+++ src/app/pipes/arabic/arabic.pipe.ts
@@ -5,9 +5,10 @@ export class ArabicPipe implements PipeTransform {
   transform(value: string): string {
     const parts = value.split('<arabic>');
     const [arabic, urduAfterArabic] = parts[1]?.split('</arabic>') ?? [];
-    const urdu = parts[0] + (urduAfterArabic || '');
-    return `${urdu} ${
+    const urduBeforeArabic = parts[0];
+    const verse = `${urduBeforeArabic || ''} ${
       arabic ? '<span class="arabic">' + arabic + '</span>' : ''
-    }`;
+    } ${urduAfterArabic || ''}`;
+    return verse;
   }
 }
